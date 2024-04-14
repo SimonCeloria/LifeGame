9e9958ca78196fe6c6197d568796e19543ce9044
unknown <celoriasimon@gmail.com>
2024-04-13 22:46:39 -0300

    add

--- main.py
+++ main.py
@@ -1,16 +1,51 @@
-# This is a sample Python script.
+# This is a following of a video from https://www.youtube.com/watch?v=qPtKv9fSHZY&ab_channel=DotCSV with
+# any corrections :)
 
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Imports
+import numpy as np
+import pygame
 
+# Init
+pygame.init()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Screen values
+width, height = 1000, 1000
+screen = pygame.display.set_mode((height, width))
 
+# Background painting
+bg = 25, 25, 25
+screen.fill(bg)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Number of cells
+nxC, nyC = 25, 25
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Cell dimensions
+dimCW = width / nxC
+dimCH = height / nyC
+
+# Cell state (1 -> alive 0 -> dead)
+gameState = np.zeros((nxC, nyC))  # np.zero creates a multidimensional array with zeros
+
+# Main loop
+while True:
+    for y in range(0, nxC):
+        for x in range(0, nyC):
+            # Calculate number of neighbors
+            n_neigh = gameState[(x - 1) % nxC, (y - 1) % nyC] + \
+                      gameState[x % nxC, (y - 1) % nyC] + \
+                      gameState[(x + 1) % nxC, (y - 1) % nyC] + \
+                      gameState[(x - 1) % nxC, y % nyC] + \
+                      gameState[(x + 1) % nxC, y % nyC] + \
+                      gameState[(x - 1) % nxC, (y + 1) % nyC] + \
+                      gameState[x % nxC, (y + 1) % nyC] + \
+                      gameState[(x + 1) % nxC, (y + 1) % nyC]
+
+            # Background grill
+            poly = [(x * dimCW, y * dimCH),
+                    ((x + 1) * dimCW, y * dimCH),
+                    ((x + 1) * dimCW, (y + 1) * dimCH),
+                    (x * dimCW, (y + 1) * dimCW)]
+            # Print grill
+            pygame.draw.polygon(screen, (128, 128, 128), poly, 1)
+    # Display game
+    pygame.display.flip()
